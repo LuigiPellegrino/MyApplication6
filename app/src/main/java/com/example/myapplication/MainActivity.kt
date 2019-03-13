1baf4dd20d4d71c4f302b6db7e2bb9ef9dba9a83
Luigi <luigi.pellegrino3@studenti.unicampania.it>
2019-03-13 17:10:19 +0100

    commit iniziale

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        try {
+            var somma1 = OP1.text.toString().toDouble()
+            var somma2 = OP2.text.toString().toDouble()
+        } catch
+
+
+    }
+
+
+
+
+}
